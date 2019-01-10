6a6cb121945f0ca413e5aae5ef4567a7be557fb4
leemch <lee7mchale@gmail.com>
2019-01-10 14:17:57 -0800

    b

--- src/App.js
+++ src/App.js
@@ -103,41 +103,7 @@ class App extends Component {
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
-/*
-  onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input})
-    fetch("http://localhost:3000/imageurl", {
-        method: "post",
-        headers: {"Content-Type": "application/json"},
 
-        body: JSON.stringify({
-        id: this.state.input
-        })
-      })
-    .then(response => response.json())
- 
-    .then(response => {
-      if(response){
-              
-      fetch("http://localhost:3000/image", {
-        method: "put",
-        headers: {"Content-Type": "application/json"},
-
-        body: JSON.stringify({
-        id: this.state.user.id
-        })
-      })
-      .then(response => response.json())
-          .then(count => this.setState(Object.assign(this.state.user, {entries: count})))
-          .catch(console.log)
-    
-
-    }
-    this.displayFaceBox(this.calculateFaceLocation(response));
-  })
-   .catch(err => console.log(err));
-  }
-*/
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
